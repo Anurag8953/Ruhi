9ca1ac69d727a6a4f99fca143463f4e1e221a381
MrNihalXd <115531374+MrNihalXd@users.noreply.github.com>
2022-10-12 08:29:26 +0530

    Update start.py

--- NihalX/plugins/Robot/start.py
+++ NihalX/plugins/Robot/start.py
@@ -188,7 +188,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAx0CbqFYzQACC7tjRiEm-4f5I2LfbTu3iZzSzkQIgwACzAYAAn5yMVbF_SVPTsDpKB4E")
+                await message.reply_sticker("CAACAgUAAx0CbqFYzQACC7xjRizobgKE6oxXVy45Rhr7ENeJqwACpwYAAnmTMFaFQhqpKkoZrh4E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
