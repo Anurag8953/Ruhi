a701c2116cd243d722e10e0bc4a98140c3898414
MrNihalXd <115531374+MrNihalXd@users.noreply.github.com>
2022-10-30 07:30:40 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/MrNihalXd/HEROKUMusix",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
